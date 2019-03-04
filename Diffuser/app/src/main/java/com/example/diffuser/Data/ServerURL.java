8ebedc93e23e15e1a5fb5d8c0f3a3fce8877a535
edicu2 <leo9333@naver.com>
2019-03-04 09:42:28 +0900

    Upload Android

---
+++ Diffuser/app/src/main/java/com/example/diffuser/Data/ServerURL.java
@@ -0,0 +1,8 @@
+package com.example.diffuser.Data;
+
+public class ServerURL {
+    public static final String MQTTURL = "tcp://broker.hivemq.com:1883"; // MQTT serverURL
+    //public static final String MQTTURL = "172.26.1.10"; // MQTT serverURL
+    public static final String MQTTTOPIC = "/demd"; // MQTT serverTopic
+
+}
---
+++ Diffuser/app/src/main/java/com/example/diffuser/Interface/Button/Aroma.java
@@ -0,0 +1,6 @@
+package com.example.diffuser.Interface.Button;
+
+public class Aroma {
+    public void start(){ System.out.println("Alarming"); }
+}
+
