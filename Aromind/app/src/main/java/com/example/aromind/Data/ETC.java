5052901cd50372d196f55ba9faa9e503c205c305
edicu2 <leo9333@naver.com>
2019-03-20 20:59:43 +0900

    Upload document( 스토리보드 )

--- Aromind/app/src/main/java/com/example/aromind/Data/ETC.java
+++
@@ -1,5 +0,0 @@
-package com.example.aromind.Data;
-
-public class ETC {
-
-}
--- Aromind/app/src/main/java/com/example/aromind/Data/URL.java
+++
@@ -1,8 +0,0 @@
-package com.example.aromind.Data;
-
-public class URL {
-    //public static final String MQTTURL = "tcp://broker.hivemq.com:1883"; // MQTT serverURL
-    public static final String MQTTURL = "tcp://172.26.1.34"; // MQTT serverURL
-    public static final String MQTTTOPIC = "/demd"; // MQTT serverTopic
-
-}
